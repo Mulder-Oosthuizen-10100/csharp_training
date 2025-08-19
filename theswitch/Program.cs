c647f8de50d2cef8dfa7c6f8226ca9cad97ee62e
Mulder Oosthuizen <muldero@pepkorit.com>
2025-08-19 14:28:29 +0200

    Initial commit - if else - switch - try catch

---
+++ theswitch/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace theswitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Enter the day of the Week (1-7): ");
+                int theday = Convert.ToInt32(Console.ReadLine());
+
+                string usersday = theday switch
+                {
+                    1 => "Monday",
+                    2 => "Tuesday",
+                    3 => "Wednesday",
+                    4 => "Thursday",
+                    5 => "Friday",
+                    _ => "Pick a day dude"
+                };
+
+                Console.WriteLine(usersday);
+            };
+
+
+            //    switch (theday)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Happy monday");
+            //            break;
+
+            //        case 2:
+            //            Console.WriteLine("Day 2 of 7...");
+            //            break;
+
+            //        case 3:
+            //            Console.WriteLine("You are now at day " + theday + " of 7...");
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Dude pick a day");
+            //            break;
+            //    }
+            //}
+
+
+
+            //Console.WriteLine("Enter the day of the Week: ");
+            //string theday = Console.ReadLine();
+
+            //if (theday == 1)
+            //{
+            //    Console.WriteLine("Happy monday");
+            //}
+            //else if (theday == 2)
+            //{
+            //    Console.WriteLine("Day two of 7...");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Dude pick a day");
+            //}
+        }
+    }
+}
